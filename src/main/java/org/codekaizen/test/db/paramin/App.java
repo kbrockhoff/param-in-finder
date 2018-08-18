c891071685704d9bbe17cca61a73cd89af560be4
Kevin Brockhoff <kbrockhoff@codekaizen.org>
2018-08-18 10:07:04 -0500

    initial setup

---
+++ src/main/java/org/codekaizen/test/db/paramin/App.java
@@ -0,0 +1,13 @@
+package org.codekaizen.test.db.paramin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
